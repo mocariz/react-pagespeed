235864a4c41b193186a85b82a336466cbf195193
monica mota ocariz <mota.ocariz@gmail.com>
2020-08-04 08:47:53 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Home from './Home';
+
+const Page = () => {
+  return <Home name="Sammy" />;
+};
+
+export default Page;
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Component = (props) => {
+  return (
+    <h1>hello {props.name}</h1>
+  );
+};
+
+export default Component;
